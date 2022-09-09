69b1daa770b5032ebcb3805bcad1e6489b1f77fb
Josa Vicente <jvicenpe@gmail.com>
2022-09-09 20:59:05 +0200

    :sparkles:

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
-import { Todo } from './classes/todo.class';
+import {Todo, TodoList} from './classes';
+// import { Todo } from './classes/todo.class';
+// import { TodoList } from './classes/todo-list.class';
+
 import './styles.css';
 
+const todoList = new TodoList();
+
 const tarea = new Todo('Aprender JS');
+
+todoList.nuevoTodo(tarea);
+
+console.log(todoList);
