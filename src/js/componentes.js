67cf723bc7161bb9163c96769d1b67235d806b4c
Josa Vicente <jvicenpe@gmail.com>
2022-09-10 22:07:58 +0200

    :sparkles:

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -8,6 +8,8 @@ import '../css/componentes.css';
 const divTodoList = document.querySelector('.todo-list')
 const txtInput = document.querySelector('.new-todo');
 const btnBorrar = document.querySelector('.clear-completed');
+const ulFiltros = document.querySelector('.filters');
+const anchorFiltros = document.querySelectorAll('.filtro');
 
     export const crearTodoHTML = ( todo ) => {
 
@@ -65,4 +67,32 @@ btnBorrar.addEventListener('click', () => {
         divTodoList
     }
     
+})
+
+ulFiltros.addEventListener('click', (event) => {
+
+    const filtro = event.target.text;
+    if (!filtro) return;
+    anchorFiltros.forEach (elem => elem.classList.remove('selected'));
+    event.target.classList.add('selected');
+
+    
+    for(const elemento of divTodoList.children){
+        elemento.classList.remove('hidden');
+        const completado = elemento.classList.contains('completed');
+        switch ( filtro ) {
+            case 'Pendientes':
+                if (completado){
+                    elemento.classList.add('hidden');
+                }
+                break;
+            case 'Completados':
+                if (!completado){
+                    elemento.classList.add('hidden');
+                }
+                break;
+            default:
+                break;
+        }
+    }
 })
